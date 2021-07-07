136c3ed6781d01257f8f588b6dfb9e89d62c0575
RMYanenko <yanenko.r.m@gmail.com>
2021-07-07 09:04:30 +0300

    create category

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-export const Categories = () => {
+interface CategoriesProps {
+  items: Array<string>;
+}
+
+export const Categories = ({ items }: CategoriesProps): JSX.Element => {
   return (
     <div className="categories">
       <ul>
         <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {items.map((item, index) => (
+          <li key={`${item}_${index}`}>{item}</li>
+        ))}
       </ul>
     </div>
   );
