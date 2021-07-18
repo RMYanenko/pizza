4b8a9b69b45d80ce3bcd836ddee67a6c66fe5b6d
RMYanenko <yanenko.r.m@gmail.com>
2021-07-18 22:47:19 +0300

    add redax

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from 'redux';
 import "./scss/app.scss";
 import App from "./App";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
